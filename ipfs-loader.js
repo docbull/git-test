6af4da04f9b755d3556e053b651843c2e44054df
docbull <fudghks21@gmail.com>
2022-05-17 00:44:14 +0900

    IPFS chunk loader running on proxy

--- ipfs-loader.js
+++ ipfs-loader.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
+// const fs = require('fs');
 const { spawn } = require('child_process');
 
+let length = 0;
+
 async function execac(command, args) {
+    length = 0;
     return new Promise(resolve => {
+        let chunk = [];
         const proc = spawn(command, args);
 
         proc.stdout.on('data', (data) => {
-            //console.log('data:', data);
+            length += data.length;
+            chunk.push(data);
         });
         
         proc.stderr.setEncoding("utf8");
@@ -15,30 +20,44 @@ async function execac(command, args) {
         });
 
         proc.on('close', () => {
-            resolve(`${command} end`);
+            resolve(chunk);
         });
     })
 }
 
-async function readChunk(file) {
+// async function readChunk(file) {
+//     return new Promise(resolve => {
+//         fs.readFile(`./${file}`, (err, data) => {
+//             if (err) {
+//                 console.error(err);
+//                 return;
+//             }
+//             let units = new Uint8Array(data);
+//             resolve(units);
+//         });
+//     })
+// }
+
+exports.catChunkFromIPFS = async function (command, args) {
+    let data = await execac(command, args);
     return new Promise(resolve => {
-        fs.readFile(`./${file}`, (err, data) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            let units = new Uint8Array(data);
-            resolve(units);
-        });
+        console.log('length:', length);
+        let value = new Uint8Array(length);
+        let offset = 0;
+        for (const buf of data) {
+            value.set(buf, offset);
+            offset += buf.length;
+        }
+        resolve(value);
     })
 }
 
-exports.getChunkFromIPFS = async function (command, args) {
-    await execac(command, args);
-    return new Promise(resolve => {
-        let chunk = args[1].split('/');
-        readChunk(chunk[1]).then(function(result) {
-            resolve(result);
-        });
-    })
-}
+// exports.getChunkFromIPFS = async function (command, args) {
+//     await execac(command, args);
+//     return new Promise(resolve => {
+//         let chunk = args[1].split('/');
+//         readChunk(chunk[1]).then(function(result) {
+//             resolve(result);
+//         });
+//     })
+// }
